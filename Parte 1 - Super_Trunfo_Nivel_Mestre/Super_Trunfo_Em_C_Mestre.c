505d45c5f177a3647e440e12c77f64c28bb715ce
David Cristiano <cristianodd3097@gmail.com>
2025-04-02 19:20:13 -0300

    Deixando o código da parte 1 mais robusto e mais facíl de ler!

--- Parte 1 - Super_Trunfo_Nivel_Mestre/Super_Trunfo_Em_C_Mestre.c
+++ Parte 1 - Super_Trunfo_Nivel_Mestre/Super_Trunfo_Em_C_Mestre.c
@@ -1,92 +1,89 @@
 #include <stdio.h>
 
 int main() {
-    // Codigo mais refinado em comparacao ao nivel novato e intermediario!
     printf("<------------------------------>\n Carta 1\n<------------------------------>\n");
 
-    char estado1[3], codigoUnico1[4], nomeDaCidade1[50];
-    int populacao1, pontosTuristicos1;
-    float area1, pib1, populationDensity1, gdpPerCapita1;
+    char state1[3], codSingle1[4], nameFromTheCity1[50];
+    int population1, pointsTourist1;
+    float area1, gdp1, populationDensity1, gdpPerCapita1;
 
-    // Valores cgar.
     printf("Digite a sigla do seu estado (ex: SP): ");
-    scanf("%2s", estado1);
+    scanf("%2s", state1);
 
     printf("Digite o codigo unico (ex: 123): ");
-    scanf("%s", codigoUnico1);
+    scanf("%s", codSingle1);
 
     printf("Digite o nome da cidade: ");
-    scanf("%s", nomeDaCidade1);
-    // Valores inteiro.
+    scanf(" %[^\n]s", nameFromTheCity1);
+
     printf("Digite o numero da populacao: ");
-    scanf("%d", &populacao1);
+    scanf("%d", &population1);
 
     printf("Digite o numero de pontos turisticos: ");
-    scanf("%d", &pontosTuristicos1);
-    // Valores float.
+    scanf("%d", &pointsTourist1);
+
     printf("Digite a area da cidade (km): ");
     scanf("%f", &area1);
 
     printf("Digite o PIB da cidade (em bilhoes): ");
-    scanf("%f", &pib1);
-    // Calculos.
-    populationDensity1 = populacao1 / area1;
-    gdpPerCapita1 = pib1 / populacao1;
+    scanf("%f", &gdp1);
 
-    printf("<------------------------------>\n Carta 2 \n<------------------------------>\n"); // Tabela em uma unica linha.
+    populationDensity1 = (area1 != 0) ? (population1 / area1) : 0;
+    gdpPerCapita1 = gdp1 / population1;
 
-    char estado2[3], codigoUnico2[4], nomeDaCidade2[50];
-    int populacao2, pontosTuristicos2;
-    float area2, pib2, populationDensity2, gdpPerCapita2;
+    printf("<------------------------------>\n Carta 2 \n<------------------------------>\n");
+
+    char state2[3], codSingle2[4], nameFromTheCity2[50];
+    int population2, pointsTourist2;
+    float area2, gdp2, populationDensity2, gdpPerCapita2;
 
     printf("Digite a sigla do seu estado (ex: SP): ");
-    scanf("%2s", estado2);
+    scanf("%2s", state2);
 
     printf("Digite o codigo unico (ex: 123): ");
-    scanf("%s", codigoUnico2);
+    scanf("%s", codSingle2);
 
     printf("Digite o nome da cidade: ");
-    scanf("%s", nomeDaCidade2);
+    scanf(" %[^\n]s", nameFromTheCity2);
 
     printf("Digite o numero da populacao: ");
-    scanf("%d", &populacao2);
+    scanf("%d", &population2);
 
     printf("Digite o numero de pontos turisticos: ");
-    scanf("%d", &pontosTuristicos2);
+    scanf("%d", &pointsTourist2);
 
     printf("Digite a area da cidade (km): ");
     scanf("%f", &area2);
 
     printf("Digite o PIB da cidade (em bilhoes): ");
-    scanf("%f", &pib2);
+    scanf("%f", &gdp2);
 
-    populationDensity2 = populacao2 / area2;
-    gdpPerCapita2 = pib2 / populacao2;
+    populationDensity2 = (area2 != 0) ? (population2 / area2) : 0;
+    gdpPerCapita2 = gdp2 / population2;
 
-    printf("\n<----------> Carta 1 <---------->\n"); // Resultado final em coluna.
+    printf("\n<----------> Carta 1 <---------->\n");
     printf("Estado: %s\nCodigo Unico: %s\nNome: %s\nPopulacao: %d\nPontos Turisticos: %d\nArea: %.2f km\nPIB: %.2f bilhoes\nDensidade Populacional: %.2f hab/km\nPIB per Capita: %.2f\n",
-           estado1, codigoUnico1, nomeDaCidade1, populacao1, pontosTuristicos1, area1, pib1, populationDensity1, gdpPerCapita1); //Declarado tudo em uma linha, assim economiza memoria!
+           state1, codSingle1, nameFromTheCity1, population1, pointsTourist1, area1, gdp1, populationDensity1, gdpPerCapita1);
 
-    printf("\n<----------> Carta 2 <---------->\n");// Resultado final em coluna.
+    printf("\n<----------> Carta 2 <---------->\n");
     printf("Estado: %s\nCodigo Unico: %s\nNome: %s\nPopulacao: %d\nPontos Turisticos: %d\nArea: %.2f km\nPIB: %.2f bilhoes\nDensidade Populacional: %.2f hab/km\nPIB per Capita: %.2f\n",
-           estado2, codigoUnico2, nomeDaCidade2, populacao2, pontosTuristicos2, area2, pib2, populationDensity2, gdpPerCapita2); //Declarado tudo em uma linha, assim economiza memoria!
+           state2, codSingle2, nameFromTheCity2, population2, pointsTourist2, area2, gdp2, populationDensity2, gdpPerCapita2);
 
     printf("<------------------------------>\n>>>>>>>>>> Resultado comparacao <<<<<<<<<<\n<------------------------------>\n");
-    //Operador ternario funcionar igual a do JS, apos <?> indica resposta verdadeira e apos <:> indica resposta em caso for falso.
-    printf("Cidade com maior populacao: %s\n", (populacao1 > populacao2) ? nomeDaCidade1 : nomeDaCidade2);
-    printf("Cidade com mais pontos turisticos: %s\n", (pontosTuristicos1 > pontosTuristicos2) ? nomeDaCidade1 : nomeDaCidade2);
-    printf("Cidade com maior area: %s\n", (area1 > area2) ? nomeDaCidade1 : nomeDaCidade2);
-    printf("Cidade com maior PIB: %s\n", (pib1 > pib2) ? nomeDaCidade1 : nomeDaCidade2);
-    printf("Cidade com maior densidade populacional: %s\n", (populationDensity1 > populationDensity2) ? nomeDaCidade1 : nomeDaCidade2);
-    printf("Cidade com maior PIB per capita: %s\n", (gdpPerCapita1 > gdpPerCapita2) ? nomeDaCidade1 : nomeDaCidade2);
+    printf("Cidade com maior populacao: %s\n", (population1 > population2) ? nameFromTheCity1 : nameFromTheCity2);
+    printf("Cidade com mais pontos turisticos: %s\n", (pointsTourist1 > pointsTourist2) ? nameFromTheCity1 : nameFromTheCity2);
+    printf("Cidade com maior area: %s\n", (area1 > area2) ? nameFromTheCity1 : nameFromTheCity2);
+    printf("Cidade com maior PIB: %s\n", (gdp1 > gdp2) ? nameFromTheCity1 : nameFromTheCity2);
+    printf("Cidade com maior densidade populacional: %s\n", (populationDensity1 > populationDensity2) ? nameFromTheCity1 : nameFromTheCity2);
+    printf("Cidade com maior PIB per capita: %s\n", (gdpPerCapita1 > gdpPerCapita2) ? nameFromTheCity1 : nameFromTheCity2);
 
     printf("<------------------------------>\n>>>>>>>>>> Soma de todos os valores! <<<<<<<<<<\n<------------------------------>\n");
 
-    double superSomaCarta1 = populacao1 + pontosTuristicos1 + area1 + pib1 + gdpPerCapita1 + (1.0 / populationDensity1); // Todos os valores somados.
-    double superSomaCarta2 = populacao2 + pontosTuristicos2 + area2 + pib2 + gdpPerCapita2 + (1.0 / populationDensity2); // Todos os valores somados.
+    double superSumCart1 = population1 + pointsTourist1 + area1 + gdp1 + gdpPerCapita1 + (1.0 / (populationDensity1 != 0 ? populationDensity1 : 1));
+    double superSumCart2 = population2 + pointsTourist2 + area2 + gdp2 + gdpPerCapita2 + (1.0 / (populationDensity2 != 0 ? populationDensity2 : 1));
 
-    printf("Carta 1 possui o valor total de: %.3f\n", superSomaCarta1);
-    printf("Carta 2 possui o valor total de: %.3f\n", superSomaCarta2);
+    printf("Carta 1 possui o valor total de: %.3f\n", superSumCart1);
+    printf("Carta 2 possui o valor total de: %.3f\n", superSumCart2);
 
     return 0;
 }
---
+++ Parte 2 - Super_Trunfo_Nivel_Mestre_Estrutura_Comparacao/README.md
@@ -0,0 +1,9 @@
+## 🔴 Nível intermediario 🔥!
+
+Comparado aos níveis anteriores, o código está mais refinado, limpo e otimizado, com uma redução significativa no número de linhas.
+
+## 👀 De olho nas mudanças❗
+
+- 🔹  Novas variáveis: Foram adicionadas duas variáveis do tipo float (somaGlobal1 e somaGlobal2) para calcular a soma de todos os valores da carta.
+- 🔹 Uso de operador ternário: Implementação de uma estrutura de decisão simples utilizando o operador ternário (? :), semelhante ao JavaScript.
+- 🔹 Código mais enxuto: Melhor organização, maior objetividade e uma visualização mais linear para facilitar a leitura e manutenção.
